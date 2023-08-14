e5716b9be60ae111df2d503cfea1ca53e052910d
surovishimu <surovi.shimu95@gmail.com>
2023-08-14 18:17:45 +0600

    my digital tasbih

---
+++ script.js
@@ -0,0 +1,35 @@
+let defaultValue1 = 0;
+
+document.getElementById('btn-count-1').addEventListener('click', function () {
+    const countField = document.getElementById('count-field-1');
+    defaultValue1++;
+    countField.innerText = defaultValue1;
+})
+document.getElementById('reset-btn-1').addEventListener('click', function () {
+    const countField = document.getElementById('count-field-1');
+    countField.innerText = '0';
+})
+
+let defaultValue2 = 0;
+document.getElementById('btn-count-2').addEventListener('click', function () {
+    const countField = document.getElementById('count-field-2');
+    defaultValue2++;
+    countField.innerText = defaultValue2;
+})
+document.getElementById('reset-btn-2').addEventListener('click', function () {
+    const countField = document.getElementById('count-field-2');
+    countField.innerText = '0';
+})
+
+
+let defaultValue3 = 0;
+document.getElementById('btn-count-3').addEventListener('click', function () {
+    const countField = document.getElementById('count-field-3');
+    defaultValue3++;
+    countField.innerText = defaultValue3;
+})
+document.getElementById('reset-btn-3').addEventListener('click', function () {
+    const countField = document.getElementById('count-field-3');
+    countField.innerText = '0';
+})
+
